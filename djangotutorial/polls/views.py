9da46b91937c8131075c84ffa02b1b69624db2e9
denis-spe <denisbrian07@gmail.com>
2025-06-01 22:12:07 +0000

    Get testing project

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -36,7 +36,7 @@ class DetailView(generic.DetailView):
     
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = "polls/detail.html"
+    template_name = "polls/results.html"
 
 # def detail(request, question_id):
 #     # try:
